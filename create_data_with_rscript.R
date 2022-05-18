3866e4aef98ffd6f22a4060f0881df9484ac5dd4
Ophir Betser <ophirbetser@ob-mba.local>
2022-05-18 18:21:40 +0300

    lets start commit local

---
+++ create_data_with_rscript.R
@@ -0,0 +1,24 @@
+
+pacman::p_load(
+            data.table,
+            tidyverse,
+            openxlsx,
+            lubridate,
+            skimr,
+            DT,
+            shiny,
+            visdat)
+
+
+dt <- fread('/Users/ophirbetser/Ophir/R PROJECTS/auto_git_update/data/my_dataset.csv')
+
+cnt <- nrow(dt)
+dt <- rbind(dt, list(lubridate::now(), cnt))
+cnt <- cnt + 1
+
+fwrite(dt, 
+       '/Users/ophirbetser/Ophir/R PROJECTS/auto_git_update/data/my_dataset.csv'
+       )
+
+
+
